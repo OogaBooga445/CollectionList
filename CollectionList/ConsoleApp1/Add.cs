3f4c5cb0c762a32ffda68c13f23da36f2508271f
OogaBooga445 <117642804+OogaBooga445@users.noreply.github.com>
2024-03-25 20:10:14 +0200

    First commit

---
+++ CollectionList/ConsoleApp1/Add.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace connect
+{
+    
+
+class Adding {
+
+
+        public static void Add() {
+
+            string filePath = "data.csv";
+
+            // Data to write to the CSV file
+            string[] data = { "John", "Doe", "30" };
+
+            // Write to the CSV file
+            using StreamWriter writer = new StreamWriter(filePath, true);
+            foreach (string value in data)
+            {
+                writer.Write(value + ",");
+            }
+            writer.WriteLine(); // Move to the next line
+            Console.WriteLine("Dati saglabāti");
+        }
+    }
+}
---
+++ CollectionList/ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel;
+namespace connect
+{
+class Program {
+
+ public static void Main(string[] args) {
+
+
+
+   Adding.Add();
+   //  Console.WriteLine("Hello, World!");
+      //    string filePath = "data.csv";
+
+      //   // Data to write to the CSV file
+      //   string[] data = { "John", "Doe", "30" };
+
+      //   // Write to the CSV file
+      //   using StreamWriter writer = new StreamWriter(filePath, true);
+      //   foreach (string value in data)
+      //   {
+      //       writer.Write(value + ",");
+      //   }
+      //   writer.WriteLine(); // Move to the next line
+
+        //   Console.WriteLine("Data written to the CSV file successfully.");
+        //     filePath = "example.csv";
+
+        //   // Read the CSV file
+        //   using StreamReader reader = new StreamReader(filePath);
+        //   while (!reader.EndOfStream)
+        //   {
+        //       string line = reader.ReadLine();
+        //       string[] values = line.Split(',');
+
+        //       // Process the values as needed
+        //       foreach (string value in values)
+        //       {
+        //           Console.Write(value + "\t");
+        //       }
+        //       Console.WriteLine();
+        //   }
+      }
+   }
+}
