fbffc93f71f9288db6288ff45716860b72f1e9e7
OogaBooga445 <117642804+OogaBooga445@users.noreply.github.com>
2024-05-15 17:27:19 +0000

    Pabeigta programma

---
+++ CollectionList/ConsoleApp1/ReadMe.md
@@ -0,0 +1,7 @@
+Instrukcija kā palast programmu.
+
+1. Lejupielādēt Visual studio code vai jebkuru citu koda redaktoru.
+2. Lejupielādēt .NET framework un C# valodas atbalstītāju.
+3. Atvērt programmu un to palaist ar Run.
+
+Šo programmu uzstādīt un palaist it ļoti viegli un grūti saskarties ar jebkādām kļūdām.
--- CollectionList/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ CollectionList/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+333f5aa8098870de3a01db17c1d8384fbd102332")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14573550c62b6974391fdef3f9f5341c3a767d79")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
